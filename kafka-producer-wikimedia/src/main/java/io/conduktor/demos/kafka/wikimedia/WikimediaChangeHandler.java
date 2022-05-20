3a1445d689e18c9909379043bd6425b7ab417a5a
Jose A. I. López <17472377+J-A-I-L@users.noreply.github.com>
2022-05-20 17:46:36 +0200

    First commit.

---
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,46 @@
+package io.conduktor.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+    KafkaProducer<String, String> kafkaProducer;
+    String topic;
+    private static final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class);
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+        // Nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        kafkaProducer.close();
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        log.info(messageEvent.getData());
+        // Asynchronous
+        kafkaProducer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s) {
+        // Nothing here
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error in Stream Reading", throwable);
+    }
+}
